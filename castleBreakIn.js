e4bd1a90a01570c1bdc5e8f7f3790566cb117db7
Weintraubm <weintraubm@dewv.edu>
2018-09-17 16:52:54 +0000

    Updates via sync script

--- castleBreakIn.js
+++ castleBreakIn.js
@@ -3,20 +3,18 @@ import {game, Sprite} from "./sgc/sgc.js";
 game.setBackground("grass.png");
 
 class Wall extends Sprite() {
-    Constructor(x,y,name,image) {
-        super();
-        this.x =
-        this.y =
-        this.name =
-        this.setImage(""); 
+    constructor(x,y,name,image) {
+        this.x = x;
+        this.y = y;
+        this.name =name;
+        this.setImage(image); 
         this.accelerateOnBounce = false;
     }
 }
 
 new Wall(0,0,"A spooky castle wall","castle.png");
 
-new Wall(0,200," Left side wall","wall.png");
+let leftWall = new Wall(0,200," Left side wall","wall.png");
 
-new Wall(game.displayWidth-48,200,"right side wall","wall,png");
+let rightWall = new Wall(game.displayWidth-48,200,"right side wall","wall,png");
 
-new Wall(game.displayHeight-48,200,"Bottom wall","wall.png");
